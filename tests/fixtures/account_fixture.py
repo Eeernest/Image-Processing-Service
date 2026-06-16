50a29d2cb6337fd184af1344b6a0ec14eadc0b2a
Eeernest <ernestgrabski16@wp.pl>
2026-06-16 14:56:00 +0200

    test(account): add unit tests for create_account method from AccountService class

--- tests/fixtures/account_fixture.py
+++ tests/fixtures/account_fixture.py
@@ -1,7 +1,11 @@
+from unittest.mock import AsyncMock
+
 import pytest
 
 from app.models.account_model import Account
 from app.repositories.account_repository import AccountDbRepository
+from app.schemas.account_schema import AccountCreate
+from app.services.account_service import AccountService
 from tests.conftest import db_session
 
 @pytest.fixture()
@@ -19,3 +23,23 @@ def account_obj():
 @pytest.fixture()
 async def saved_account_obj(db_repo, account_obj):
   return await db_repo.save(account_obj)
+
+@pytest.fixture()
+def mock_security():
+  return AsyncMock()
+
+@pytest.fixture()
+def mock_db_repo():
+  return AsyncMock()
+
+@pytest.fixture()
+def service(mock_security, mock_db_repo):
+  return AccountService(mock_security, mock_db_repo)
+
+@pytest.fixture()
+def account_data():
+  return AccountCreate(
+    username="user1",
+    email="user1@example.com",
+    password="Password123"
+  )
