c8a53cd96284e4c0d80f447e53121b617216a85d
Eeernest <ernestgrabski16@wp.pl>
2026-06-16 20:35:52 +0200

    potential GitHub Actions Config related fix

--- app/core/config.py
+++ app/core/config.py
@@ -3,11 +3,11 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Config(BaseSettings):
   POSTGRES_URL: str
 
-  ADMIN_USERNAME: str
+  ADMIN_USERNAME: str = "admin_username"
 
-  ADMIN_EMAIL: str
+  ADMIN_EMAIL: str = "admin_email"
 
-  ADMIN_HASHED_PASSWORD: str
+  ADMIN_HASHED_PASSWORD: str = "admin_hashed_password"
 
   model_config = SettingsConfigDict(
     env_file=".env",
