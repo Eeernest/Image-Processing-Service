c5ac1c918a03d4d304f0076ae67801d117a94366
Eeernest <ernestgrabski16@wp.pl>
2026-06-16 12:16:09 +0200

    GitHub Actions initial setup

--- tests/repositories/test_account_repository.py
+++ tests/repositories/test_account_repository.py
@@ -4,6 +4,7 @@ from app.models.account_model import AccountRole
 from tests.fixtures.account_fixture import db_repo, account_obj, saved_account_obj
 
 @pytest.mark.anyio
+@pytest.mark.integration
 async def test_save_succes(db_repo, account_obj):
   result = await db_repo.save(account_obj)
 
@@ -15,12 +16,14 @@ async def test_save_succes(db_repo, account_obj):
   assert result.is_deleted == False
 
 @pytest.mark.anyio
+@pytest.mark.integration
 async def test_get_by_username_success(db_repo, saved_account_obj):
   result = await db_repo.get_by_username(saved_account_obj.username)
 
   assert result.username == saved_account_obj.username
 
 @pytest.mark.anyio
+@pytest.mark.integration
 async def test_get_by_email_success(db_repo, saved_account_obj):
   result = await db_repo.get_by_email(saved_account_obj.email)
 
