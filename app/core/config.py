fb2311b46ede12f9bced3577279e11aee3c24182
Eeernest <ernestgrabski16@wp.pl>
2026-06-28 17:47:28 +0200

    update(config): add S3 credentials to Config class

--- app/core/config.py
+++ app/core/config.py
@@ -17,6 +17,14 @@ class Config(BaseSettings):
 
   ACCESS_TOKEN_EXPIRE_MINUTES: int = 0
 
+  S3_BUCKET_NAME: str = "s3_bucket_name"
+
+  S3_BUCKET_REGION: str = "s3_bucket_region"
+
+  S3_ACCESS_KEY_ID: str = "s3_access_key"
+
+  S3_SECRET_ACCESS_KEY: str = "s3-secret_access_key"
+
   model_config = SettingsConfigDict(
     env_file=".env",
     extra="ignore",
