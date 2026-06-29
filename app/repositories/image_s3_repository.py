2265cdf0d8ffc3c65c2a464f7bc9b33ef33df534
Eeernest <ernestgrabski16@wp.pl>
2026-06-29 17:33:12 +0200

    update(image): update method from ImageS3Repository class to async

--- app/repositories/image_s3_repository.py
+++ app/repositories/image_s3_repository.py
@@ -1,6 +1,7 @@
 from io import BytesIO
 import mimetypes
 
+from fastapi.concurrency import run_in_threadpool
 from types_boto3_s3 import S3Client
 
 from app.core.config import settings
@@ -9,15 +10,14 @@ class ImageS3Repository:
   def __init__(self, s3_client: S3Client):
     self.s3_client = s3_client
 
-  def upload_to_s3(self, file_bytes: bytes, key: str, content_type: str | None = None) -> str:
+  async def upload_to_s3(self, file_bytes: bytes, key: str, content_type: str | None = None) -> None:
     if content_type is None:
       content_type = mimetypes.guess_type(key)[0] or "image/jpeg"
 
-    self.s3_client.upload_fileobj(
+    await run_in_threadpool(
+      self.s3_client.upload_fileobj,
       BytesIO(file_bytes),
       settings.S3_BUCKET_NAME,
       key,
       ExtraArgs={"ContentType": content_type}
     )
-
-    return key
