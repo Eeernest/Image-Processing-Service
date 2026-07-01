2997dda302fe26231952a0af1db22d1deb8cda59
Eeernest <ernestgrabski16@wp.pl>
2026-07-01 20:16:33 +0200

    update(image): add try/except to catch IntegrityError in save method from ImageDbRepository class

--- app/repositories/image_db_repository.py
+++ app/repositories/image_db_repository.py
@@ -1,3 +1,4 @@
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models.image_model import Image
@@ -7,8 +8,14 @@ class ImageDbRepository:
     self.session = session
 
   async def save(self, image_obj: Image) -> Image:
-    self.session.add(image_obj)
-    await self.session.commit()
-    await self.session.refresh(image_obj)
+    try:
+      self.session.add(image_obj)
+      await self.session.commit()
+      await self.session.refresh(image_obj)
 
-    return image_obj
+      return image_obj
+
+    except IntegrityError as exc:
+      await self.session.rollback()
+
+      raise exc
