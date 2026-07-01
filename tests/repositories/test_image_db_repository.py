57be843f1fcafb6505089eaf25089382e5c85910
Eeernest <ernestgrabski16@wp.pl>
2026-07-01 22:20:56 +0200

    test(image): add integration test for save method from ImageDbRepository class

---
+++ tests/repositories/test_image_db_repository.py
@@ -0,0 +1,9 @@
+import pytest
+
+@pytest.mark.anyio
+@pytest.mark.integration
+async def test_save(db_repo, image_obj, saved_account_obj):
+  result = await db_repo.save(image_obj)
+
+  assert result.id is not None
+  assert result.account_id == saved_account_obj.id
