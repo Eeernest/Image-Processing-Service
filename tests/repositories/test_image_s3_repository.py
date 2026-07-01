73fdb63884f606c95b6d11484279a098f2c8bb56
Eeernest <ernestgrabski16@wp.pl>
2026-07-01 22:19:09 +0200

    test(image): add integration tset for delete_from_s3 method from ImageS3Repsitory

--- tests/repositories/test_image_s3_repository.py
+++ tests/repositories/test_image_s3_repository.py
@@ -1,8 +1,7 @@
 import os
 
+from botocore.exceptions import ClientError
 import pytest
-
-from tests.fixtures.image_fixture import s3_repo, file_bytes, key
 
 @pytest.mark.anyio
 @pytest.mark.integration
@@ -10,25 +9,17 @@ async def test_upload_to_s3_success(mocked_aws, s3_repo, file_bytes, key):
   await s3_repo.upload_to_s3(file_bytes, key, "image/jpeg")
   result = mocked_aws.get_object(Bucket=os.environ["S3_BUCKET_NAME"], Key=key)
 
-  assert result["Body"].read() == file_bytes
   assert result["ContentType"] == "image/jpeg"
 
 @pytest.mark.anyio
 @pytest.mark.integration
-async def test_upload_to_s3_guess_content_type(mocked_aws, s3_repo, file_bytes):
-  png_key = "uploads/test_image/image.png"
+async def test_delete_from_s3(mocked_aws, s3_repo, file_bytes, key):
+  await s3_repo.upload_to_s3(file_bytes, key, "image/jpeg")
 
-  await s3_repo.upload_to_s3(file_bytes=file_bytes, key=png_key)
-  result = mocked_aws.get_object(Bucket=os.environ["S3_BUCKET_NAME"], Key=png_key)
+  result = await s3_repo.delete_from_s3(key)
 
-  assert result["ContentType"] == "image/png"
-
-@pytest.mark.anyio
-@pytest.mark.integration
-async def test_upload_to_s3_fallback_content_type(mocked_aws, s3_repo, file_bytes):
-  unknown_key = "unknown_key"
-
-  await s3_repo.upload_to_s3(file_bytes, unknown_key)
-  result = mocked_aws.get_object(Bucket=os.environ["S3_BUCKET_NAME"], Key=unknown_key)
-
-  assert result["ContentType"] == "image/jpeg"
+  with pytest.raises(ClientError) as exc:
+        mocked_aws.get_object(Bucket=os.environ["S3_BUCKET_NAME"], Key=key)
+        
+  assert result == {"message": "Image deleted"}
+  assert exc.value.response["Error"]["Code"] == "NoSuchKey"
