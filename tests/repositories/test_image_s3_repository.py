b57a1b2bb7f139b29039cfd18626198e33a42379
Eeernest <ernestgrabski16@wp.pl>
2026-07-04 14:36:14 +0200

    test(image): add integration test for download_from_s3 method from ImageS3Repository class

--- tests/repositories/test_image_s3_repository.py
+++ tests/repositories/test_image_s3_repository.py
@@ -23,3 +23,12 @@ async def test_delete_from_s3(mocked_aws, image_s3_repo, file_bytes, key):
         
   assert result == {"message": "Image deleted"}
   assert exc.value.response["Error"]["Code"] == "NoSuchKey"
+
+@pytest.mark.anyio
+@pytest.mark.integration
+async def test_download_from_s3(image_s3_repo, file_bytes, key):
+   await image_s3_repo.upload_to_s3(file_bytes, key, "image/jpeg")
+
+   result = await image_s3_repo.download_from_s3(key)
+
+   assert result == file_bytes
