c25036f610a675799e9a18cc5f664d192ac8ccb2
Eeernest <ernestgrabski16@wp.pl>
2026-07-08 11:34:48 +0200

    fix(image): fix performance issues related with using file.read() in ImageService class

--- app/services/image_service.py
+++ app/services/image_service.py
@@ -2,10 +2,11 @@ from io import BytesIO
 
 from botocore.exceptions import ClientError, BotoCoreError
 from fastapi import UploadFile
+from fastapi.concurrency import run_in_threadpool
 from PIL import Image as PILImage, UnidentifiedImageError
 from sqlalchemy.exc import IntegrityError
 
-from app.core.exceptions import MaxFileSizeExceededException, ImageResolutionException, InvalidImageFormatException, S3UploadFailedException, DuplicateImageException
+from app.core.exceptions import MaxFileSizeExceededException, ImageResolutionException, InvalidImageFormatException, S3UploadFailedException, DuplicateImageException, ImageNotFoundException
 from app.models.image_model import Image
 from app.repositories.image_db_repository import ImageDbRepository
 from app.repositories.image_s3_repository import ImageS3Repository
@@ -15,19 +16,17 @@ class ImageService:
     self.db_repo = db_repo
     self.s3_repo = s3_repo
 
-  def _validate_file_size(self, file_bytes: bytes) -> int:
+  def _validate_file_size(self, file_size: int) -> int:
     max_file_size = 10 * 1024 * 1024
 
-    file_size_bytes = len(file_bytes)
-
-    if file_size_bytes > max_file_size:
+    if file_size > max_file_size:
       raise MaxFileSizeExceededException()
 
-    return file_size_bytes
+    return file_size
 
-  def _validate_image(self, file_bytes: bytes) -> str:
+  def _validate_image(self, file_obj) -> str:
     try:
-      with PILImage.open(BytesIO(file_bytes)) as img:
+      with PILImage.open(file_obj) as img:
         allowed_image_formats = ["PNG", "JPEG", "WEBP"]
         max_width = 5000
         max_height = 5000
@@ -47,18 +46,16 @@ class ImageService:
 
   async def upload_image(self, account_id: int, file: UploadFile) -> Image:
     filename = file.filename
-    file_bytes = await file.read()
-    file_size_bytes = self._validate_file_size(file_bytes)
+    file_size_bytes = self._validate_file_size(file.size)
     content_type = file.content_type
 
-    detected_format = self._validate_image(file_bytes)
-
+    detected_format = await run_in_threadpool(self._validate_image, file.file)
     generated_key = f"account/{account_id}/images/{filename}"
 
     try:
-      await self.s3_repo.upload_to_s3(file_bytes, generated_key, content_type)
+      await self.s3_repo.upload_to_s3(file.file, generated_key, content_type)
 
-    except (ClientError,BotoCoreError) as e:
+    except (ClientError,BotoCoreError):
       raise S3UploadFailedException()
 
     image_obj = Image(
