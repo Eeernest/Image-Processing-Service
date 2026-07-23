de9f8e659ed13982ff11fc568a4a3f80322fd210
Eeernest <ernestgrabski16@wp.pl>
2026-07-23 20:21:28 +0200

    feat(image): add resize_image function to image_router

--- app/routers/image_router.py
+++ app/routers/image_router.py
@@ -9,3 +9,7 @@ router = APIRouter()
 @router.post("/upload_image", response_model=ImageRead)
 async def upload_image(user: CurrentUserDep, service: ImageServiceDep, file: UploadFile):
   return await service.upload_image(user.id, file)
+
+@router.get("/resize_image/{image_id}", response_model=ImageRead)
+async def resize_image(user: CurrentUserDep, service: ImageServiceDep, image_id: int, width: int, height: int):
+  return await service.resize_image(user.id, image_id, width, height)
