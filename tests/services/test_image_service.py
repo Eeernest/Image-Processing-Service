5d41fa1b69d279b170e825b4222fb0a55f42e051
Eeernest <ernestgrabski16@wp.pl>
2026-07-24 20:12:07 +0200

    test(image): add unit tests for crop_center_image method from ImageService class

--- tests/services/test_image_service.py
+++ tests/services/test_image_service.py
@@ -191,3 +191,92 @@ async def test_resize_image_race_condition(image_obj, mock_image_db_repo, mock_i
   assert exc.value.status_code == DuplicateImageException.status_code
   assert exc.value.detail == DuplicateImageException.detail
   assert mock_image_db_repo.save.call_count == 1
+
+@pytest.mark.anyio
+@pytest.mark.unit
+async def test_crop_center_image_success(image_obj, mock_image_db_repo, mock_image_s3_repo, image_service, mock_file_like):
+  mock_image_db_repo.get_by_id.return_value = image_obj
+  mock_image_s3_repo.download_from_s3.return_value = mock_file_like
+  mock_image_db_repo.save.return_value = image_obj
+  mock_image_s3_repo.upload_to_s3.return_value = None
+
+  result = await image_service.crop_center_image(image_obj.account_id, 1, 12, 34)
+
+  assert result == image_obj
+  assert mock_image_db_repo.save.call_count == 1
+  assert mock_image_s3_repo.upload_to_s3.call_count == 1
+
+@pytest.mark.anyio
+@pytest.mark.unit
+async def test_crop_center_image_not_found_exception(mock_image_db_repo, image_service):
+  mock_image_db_repo.get_by_id.side_effect = ImageNotFoundException()
+
+  with pytest.raises(ImageNotFoundException) as exc:
+    await image_service.crop_center_image(1, 12, 34, 34)
+
+  assert exc.value.status_code == ImageNotFoundException.status_code
+  assert exc.value.detail == ImageNotFoundException.detail
+
+@pytest.mark.anyio
+@pytest.mark.unit
+async def test_crop_center_image_user_not_found_exception(image_obj, mock_image_db_repo, image_service):
+  image_obj.account_id = 1
+
+  mock_image_db_repo.get_by_id.return_value = image_obj
+
+  with pytest.raises(UserNotFoundException) as exc:
+    await image_service.crop_center_image(12, 3, 23, 34)
+
+  assert exc.value.status_code == UserNotFoundException.status_code
+  assert exc.value.detail == UserNotFoundException.detail
+
+@pytest.mark.anyio
+@pytest.mark.unit
+async def test_crop_center_image_s3_download_s3_exception(image_obj, mock_image_db_repo, mock_image_s3_repo, image_service):
+  mock_image_db_repo.get_by_id.return_value = image_obj
+  mock_image_s3_repo.download_from_s3.side_effect = S3DownloadFailedException()
+
+  with pytest.raises(S3DownloadFailedException) as exc:
+    await image_service.crop_center_image(image_obj.account_id, 1, 12, 23)
+
+  assert exc.value.status_code == S3DownloadFailedException.status_code
+  assert exc.value.detail == S3DownloadFailedException.detail
+
+@pytest.mark.anyio
+@pytest.mark.unit
+async def test_crop_center_image_too_small_exception(image_obj, mock_image_db_repo, mock_image_s3_repo, image_service, mock_file_like):
+  mock_image_db_repo.get_by_id.return_value = image_obj
+  mock_image_s3_repo.download_from_s3.return_value = mock_file_like
+
+  with pytest.raises(ImageTooSmallException) as exc:
+    await image_service.crop_center_image(image_obj.account_id, 1, 200, 300)
+
+  assert exc.value.status_code == ImageTooSmallException.status_code
+  assert exc.value.detail == ImageTooSmallException.detail
+
+@pytest.mark.anyio
+@pytest.mark.unit
+async def test_crop_center_image_race_condition(image_obj, mock_image_db_repo, mock_image_s3_repo, image_service, mock_file_like):
+  mock_image_db_repo.get_by_id.return_value = image_obj
+  mock_image_s3_repo.download_from_s3.return_value = mock_file_like
+  mock_image_db_repo.save.side_effect = IntegrityError("stmt", "params", "s3_key")
+
+  with pytest.raises(DuplicateImageException) as exc:
+    await image_service.crop_center_image(image_obj.account_id, 1, 34, 12)
+
+  assert exc.value.status_code == DuplicateImageException.status_code
+  assert exc.value.detail == DuplicateImageException.detail
+
+@pytest.mark.anyio
+@pytest.mark.unit
+async def test_crop_center_image_s3_upload_exception(image_obj, mock_image_db_repo, mock_image_s3_repo, image_service, mock_file_like):
+  mock_image_db_repo.get_by_id.return_value = image_obj
+  mock_image_s3_repo.download_from_s3.return_value = mock_file_like
+  mock_image_db_repo.save.return_value = image_obj
+  mock_image_s3_repo.upload_to_s3.side_effect = S3UploadFailedException()
+
+  with pytest.raises(S3UploadFailedException) as exc:
+    await image_service.crop_center_image(image_obj.account_id, 12, 33, 33)
+
+  assert exc.value.status_code == S3UploadFailedException.status_code
+  assert exc.value.detail == S3UploadFailedException.detail
