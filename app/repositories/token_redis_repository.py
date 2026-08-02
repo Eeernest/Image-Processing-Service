6e4ba4fccc39370a9a02b8c1125b5309fc600361
Eeernest <ernestgrabski16@wp.pl>
2026-08-02 15:25:56 +0200

    refactor(token): change argument name to sub in store_refresh_token method in TokenRedisRepository class

--- app/repositories/token_redis_repository.py
+++ app/repositories/token_redis_repository.py
@@ -4,8 +4,8 @@ class TokenRedisRepository:
   def __init__(self, client: Redis):
     self.client = client
 
-  async def store_refresh_token(self, jti: str, expires_delta: int, account_id: str ) -> None:
-    await self.client.setex(f"refresh_token:{jti}", expires_delta, account_id)
+  async def store_refresh_token(self, jti: str, expires_delta: int, sub: str ) -> None:
+    await self.client.setex(f"refresh_token:{jti}", expires_delta, sub)
 
   async def get_refresh_token(self, jti: str) -> str | None:
     return await self.client.get(f"refresh_token:{jti}")
