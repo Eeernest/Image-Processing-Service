7049fe384060bc800891b591fdde434565e067d6
Eeernest <ernestgrabski16@wp.pl>
2026-08-02 17:29:40 +0200

    refactor(token): change method name from get_refresh_token to get_account_id for better clarity

--- app/repositories/token_redis_repository.py
+++ app/repositories/token_redis_repository.py
@@ -7,5 +7,5 @@ class TokenRedisRepository:
   async def store_refresh_token(self, jti: str, expires_delta: int, sub: str ) -> None:
     await self.client.setex(f"refresh_token:{jti}", expires_delta, sub)
 
-  async def get_refresh_token(self, jti: str) -> str | None:
+  async def get_account_id(self, jti: str) -> str | None:
     return await self.client.get(f"refresh_token:{jti}")
--- tests/repositories/test_token_redis_repository.py
+++ tests/repositories/test_token_redis_repository.py
@@ -2,9 +2,9 @@ import pytest
 
 @pytest.mark.anyio
 @pytest.mark.integration
-async def test_store_and_get_refresh_token_success(token_redis_repo, refresh_token_data):
+async def test_store_refresh_token_and_get_account_id_success(token_redis_repo, refresh_token_data):
   await token_redis_repo.store_refresh_token(refresh_token_data["jti"], refresh_token_data["exp"] * 86400, refresh_token_data["sub"])
 
-  result = await token_redis_repo.get_refresh_token(refresh_token_data["jti"])
+  result = await token_redis_repo.get_account_id(refresh_token_data["jti"])
 
   assert result == refresh_token_data["sub"]
