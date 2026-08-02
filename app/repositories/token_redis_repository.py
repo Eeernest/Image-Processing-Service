8c41492c8d6bf91e47d9319608fcb60b6fe03ac7
Eeernest <ernestgrabski16@wp.pl>
2026-08-02 15:05:57 +0200

    feat(token): ada get_refresh_token method to TokenRedisRepository class

--- app/repositories/token_redis_repository.py
+++ app/repositories/token_redis_repository.py
@@ -4,5 +4,8 @@ class TokenRedisRepository:
   def __init__(self, client: Redis):
     self.client = client
 
-  async def save(self, jti: str, expires_delta: int, account_id: str) -> None:
-    await self.client.setex(name=f"refresh_token:{jti}", time=expires_delta, value=account_id)
+  async def store_refresh_token(self, jti: str, expires_delta: int, account_id: str ) -> None:
+    await self.client.setex(f"refresh_token:{jti}", expires_delta, account_id)
+
+  async def get_refresh_token(self, jti: str) -> str | None:
+    return await self.client.get(f"refresh_token:{jti}")
