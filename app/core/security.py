666f45e4a8e7956ff03b4e7b1fe98ac6558b5316
Eeernest <ernestgrabski16@wp.pl>
2026-08-02 11:31:57 +0200

    feat(security): add create_access_token and create_refresh_token methods

--- app/core/security.py
+++ app/core/security.py
@@ -1,7 +1,12 @@
+from datetime import datetime, timedelta, timezone
+import uuid
+
 from fastapi.concurrency import run_in_threadpool
 from fastapi.security import OAuth2PasswordBearer
 import jwt
 from pwdlib import PasswordHash
+
+from app.core.config import settings
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
@@ -12,11 +17,52 @@ class Security:
   async def get_password_hash(self, password: str) -> str:
     return await run_in_threadpool(self.hasher.hash, password)
   
-  async def verify_password(self, password: str, hashed_password: str) -> str:
+  async def verify_password(self, password: str, hashed_password: str) -> bool:
     return await run_in_threadpool(self.hasher.verify, password, hashed_password)
+
+  def create_access_token(self, account_data: dict, expires_delta: timedelta | None = None) -> str:
+    payload = {
+      "sub": account_data.get("sub"),
+      "role": account_data.get("role"),
+      "exp": self._get_exp_minutes(expires_delta),
+      "jti": str(uuid.uuid4()),
+      "refresh": False
+    }
+
+    token = jwt.encode(payload, settings.SECRET_KEY, settings.ALGORITHM)
+
+    return token
+
+  def create_refresh_token(self, account_data: dict, expires_delta: timedelta | None = None) -> str:
+      payload = {
+        "sub": account_data.get("sub"),
+        "exp": self._get_exp_days(expires_delta),
+        "jti": str(uuid.uuid4()),
+        "refresh": True
+      }
   
-  def encode_jwt(self, to_encode: dict, secret_key: str, algorithm: str) -> str:
-    return jwt.encode(to_encode, secret_key, algorithm)
+      token = jwt.encode(payload, settings.SECRET_KEY, settings.ALGORITHM)
   
-  def decode_jwt(self, token: str, secret_key: str, algorithm: str) -> dict:
-    return jwt.decode(token, secret_key, algorithm)
+      return token
+
+  def decode_jwt(self, token: str) -> dict:
+    try:
+      return jwt.decode(token, settings.SECRET_KEY, settings.ALGORITHM)
+
+    # lateron delete this try except and do it in service
+
+    except jwt.PyJWTError as exc:
+      raise exc
+
+
+  def _get_exp_minutes(self, expires_delta: timedelta | None) -> datetime:
+    if expires_delta is None:
+      expires_delta = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    return datetime.now(timezone.utc) + expires_delta
+
+  def _get_exp_days(self, expires_delta: timedelta | None) -> datetime:
+    if expires_delta is None:
+      expires_delta = timedelta(days=settings.REFRESH_TOKEN_EXPIRE_DAYS)
+
+    return datetime.now(timezone.utc) + expires_delta
