158a231412e46cbbe8f20dd12c8ffd7d7bc6b95a
Eeernest <ernestgrabski16@wp.pl>
2026-08-02 18:00:05 +0200

    feat(token): add delete_refresh_token method to TokenRedisRepository class

--- app/repositories/token_redis_repository.py
+++ app/repositories/token_redis_repository.py
@@ -5,7 +5,15 @@ class TokenRedisRepository:
     self.client = client
 
   async def store_refresh_token(self, jti: str, expires_delta: int, sub: str ) -> None:
-    await self.client.setex(f"refresh_token:{jti}", expires_delta, sub)
+    await self.client.setex(self._get_key(jti), expires_delta, sub)
 
   async def get_account_id(self, jti: str) -> str | None:
-    return await self.client.get(f"refresh_token:{jti}")
+    return await self.client.get(self._get_key(jti))
+
+  async def delete_refresh_token(self, jti: str) -> None:
+    return await self.client.delete(self._get_key(jti))
+
+
+
+  def _get_key(self, jti: str) -> str:
+    return f"refresh_token:{jti}"
