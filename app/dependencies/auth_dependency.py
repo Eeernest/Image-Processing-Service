7227d603932011d54ea700f360409b872ce8138b
Eeernest <ernestgrabski16@wp.pl>
2026-08-03 12:44:08 +0200

    update(auth): add token_redis_repository to AuthServiceDep

--- app/dependencies/auth_dependency.py
+++ app/dependencies/auth_dependency.py
@@ -4,13 +4,16 @@ from fastapi import Depends
 
 from app.core.security import Security
 from app.db.database import SessionDep
+from app.redis.redis_client import RedisDep
 from app.repositories.account_db_repository import AccountDbRepository
+from app.repositories.token_redis_repository import TokenRedisRepository
 from app.services.auth_service import AuthService
 
-def get_auth_service(session: SessionDep):
+def get_auth_service(session: SessionDep, client: RedisDep):
   security = Security()
   db_repo = AccountDbRepository(session)
+  redis_repo = TokenRedisRepository(client)
 
-  return AuthService(security, db_repo)
+  return AuthService(security, db_repo, redis_repo)
 
 AuthServiceDep = Annotated[AuthService, Depends(get_auth_service)]
