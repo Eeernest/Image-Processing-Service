a6e128da11b782a8a61bf477db93f6d8e4753291
Eeernest <ernestgrabski16@wp.pl>
2026-08-05 18:09:18 +0200

    test(image): add integration tests for get_all_by_account_id method from ImageDbRepository class

--- tests/repositories/test_image_db_repository.py
+++ tests/repositories/test_image_db_repository.py
@@ -1,5 +1,7 @@
 import pytest
 
+from app.schemas.image_schema import ImageFormat
+
 @pytest.mark.anyio
 @pytest.mark.integration
 async def test_save(image_db_repo, image_obj, saved_account_obj):
@@ -21,3 +23,19 @@ async def test_get_by_id(image_db_repo, saved_image_obj):
   result = await image_db_repo.get_by_id(saved_image_obj.id)
 
   assert result == saved_image_obj
+
+@pytest.mark.anyio
+@pytest.mark.integration
+async def test_get_all_by_account_id(image_db_repo, saved_image_obj):
+  result = await image_db_repo.get_all_by_account_id(saved_image_obj.account_id, 0, 10)
+
+  assert len(result) == 1
+  assert saved_image_obj in result
+
+@pytest.mark.anyio
+@pytest.mark.integration
+async def test_get_all_by_account_id_file_format(image_db_repo, saved_image_obj):
+  result = await image_db_repo.get_all_by_account_id(saved_image_obj.account_id, 0, 10, ImageFormat.WEBP)
+
+  assert len(result) == 0
+  assert saved_image_obj not in result
