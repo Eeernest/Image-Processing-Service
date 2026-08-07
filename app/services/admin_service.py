e4ff2cf553af63fed22b8f56ff1b932bc559207f
Eeernest <ernestgrabski16@wp.pl>
2026-08-07 20:37:04 +0200

    update(admin): add offset and limit arguments to view_all_accounts method from AdminService class

--- app/services/admin_service.py
+++ app/services/admin_service.py
@@ -6,12 +6,12 @@ class AdminService:
   def __init__(self, db_repo: AccountDbRepository):
     self.db_repo = db_repo
 
-  async def view_all_accounts(self, is_active: bool | None = None, is_deleted: bool | None = None) -> list[Account]:
+  async def view_all_accounts(self, offset: int, limit: int, is_active: bool | None = None, is_deleted: bool | None = None) -> list[Account]:
     return await self._try_get_all_accounts(is_active, is_deleted)
 
 
-  async def _try_get_all_accounts(self, is_active: bool | None = None, is_deleted: bool | None = None) -> list[Account]:
-    account_obj_list = await self.db_repo.get_all_accounts(is_active, is_deleted)
+  async def _try_get_all_accounts(self, offset: int, limit: int, is_active: bool | None = None, is_deleted: bool | None = None) -> list[Account]:
+    account_obj_list = await self.db_repo.get_all_accounts(offset, limit, is_active, is_deleted)
 
     if len(account_obj_list) == 0:
       raise e.UserNotFoundException()
--- tests/services/test_admin_service.py
+++ tests/services/test_admin_service.py
@@ -8,7 +8,7 @@ from app.models.account_model import AccountRole
 async def test_view_all_accounts_success(mock_admin_db_repo, admin_service, mock_admin_account_list):
   mock_admin_db_repo.get_all_accounts.return_value = mock_admin_account_list
 
-  result = await admin_service.view_all_accounts()
+  result = await admin_service.view_all_accounts(0, 10)
 
   assert len(result) == 3
   assert result == mock_admin_account_list
@@ -18,7 +18,7 @@ async def test_view_all_accounts_success(mock_admin_db_repo, admin_service, mock
 async def test_view_all_accounts_is_active_filter_success(mock_admin_db_repo, admin_service, mock_admin_account_list):
   mock_admin_db_repo.get_all_accounts.return_value = [mock_admin_account_list[1]]
 
-  result = await admin_service.view_all_accounts(False)
+  result = await admin_service.view_all_accounts(0, 10, False)
 
   assert len(result) == 1
   assert mock_admin_account_list[1] in result
@@ -28,18 +28,18 @@ async def test_view_all_accounts_is_active_filter_success(mock_admin_db_repo, ad
 async def test_view_all_accounts_is_deketed_filter_success(mock_admin_db_repo, admin_service, mock_admin_account_list):
   mock_admin_db_repo.get_all_accounts.return_value = [mock_admin_account_list[2]]
 
-  result = await admin_service.view_all_accounts(None, True)
+  result = await admin_service.view_all_accounts(0, 10, None, True)
 
   assert len(result) == 1
   assert mock_admin_account_list[2] in result
 
 @pytest.mark.anyio
 @pytest.mark.unit
-async def test_view_all_accounts_user_not_found_exception(mock_admin_db_repo, admin_service):
+async def test_view_all_accounts_user_not_found_exception(mock_admin_db_repo, admin_service, mock_admin_account_list):
   mock_admin_db_repo.get_all_accounts.return_value = []
 
   with pytest.raises(e.UserNotFoundException) as exc:
-    await admin_service.view_all_accounts()
+    await admin_service.view_all_accounts(0, 10)
 
   assert exc.value.status_code == e.UserNotFoundException.status_code
   assert exc.value.detail == e.UserNotFoundException.detail
