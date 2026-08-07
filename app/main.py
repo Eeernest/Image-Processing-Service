e78f4820a5679df0d05bee13b7763ec5dda0f244
Eeernest <ernestgrabski16@wp.pl>
2026-08-07 21:51:56 +0200

    feat(admin): add view_all_accounts function to admin_router

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from fastapi.exceptions import RequestValidationError
 from app.core.exceptions import AppBaseException
 from app.core.exception_handler import custom_exc_handler, validation_exc_handler
 from app.routers.account_router import router as account_router
+from app.routers.admin_router import router as admin_router
 from app.routers.auth_router import router as auth_router
 from app.routers.image_router import router as image_router
 from app.routers.permit_router import router as permit_router
@@ -14,6 +15,7 @@ app.add_exception_handler(AppBaseException, custom_exc_handler)
 app.add_exception_handler(RequestValidationError, validation_exc_handler)
 
 app.include_router(account_router)
+app.include_router(admin_router)
 app.include_router(auth_router)
 app.include_router(image_router)
 app.include_router(permit_router)
---
+++ app/routers/admin_router.py
@@ -0,0 +1,20 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Query
+
+from app.dependencies.admin_dependency import AdminServiceDep
+from app.dependencies.permit_dependency import CurrentAdminDep
+from app.schemas.admin_schema import AdminRead
+
+router = APIRouter()
+
+@router.get("/view_all_accounts", response_model=list[AdminRead])
+async def view_all_accounts(
+  admin: CurrentAdminDep,
+  service: AdminServiceDep,
+  offset: int = 0,
+  limit: Annotated[int, Query(le=100)] = 100,
+  is_active: bool | None = None,
+  is_deleted: bool | None = None
+):
+  return await service.view_all_accounts(offset, limit, is_active, is_deleted)
