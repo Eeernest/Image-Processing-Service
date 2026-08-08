bf18738a15531993be5a15426c7147623f672a3e
Eeernest <ernestgrabski16@wp.pl>
2026-08-08 12:46:25 +0200

    test(admin): add unit tests for view_all_accounts function from admin_router

--- tests/routers/test_admin_router.py
+++ tests/routers/test_admin_router.py
@@ -1,10 +1,25 @@
 import pytest
 
+import app.core.exceptions as e
+
 @pytest.mark.anyio
 @pytest.mark.unit
 async def test_view_all_accounts_succses(mock_admin_account_list, mock_admin_service, unit_admin_client):
   mock_admin_service.view_all_accounts.return_value = mock_admin_account_list
 
   result = await unit_admin_client.get("/view_all_accounts", params={"offset": 0, "limit": 100})
+  data = result.json()
 
   assert result.status_code == 200
+  assert len(data) == 3
+
+@pytest.mark.anyio
+@pytest.mark.unit
+async def test_view_all_accounts_user_not_fount_exception(mock_admin_service, unit_admin_client):
+  mock_admin_service.view_all_accounts.side_effect = e.UserNotFoundException()
+
+  result = await unit_admin_client.get("/view_all_accounts", params={"offset": 10, "limit": 100})
+  data = result.json()
+
+  assert result.status_code == 404
+  assert data["detail"] == e.UserNotFoundException.detail
