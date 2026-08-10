6915ad3962fd94e848d977c61968a3f6e5d11c9c
Eeernest <ernestgrabski16@wp.pl>
2026-08-10 19:53:23 +0200

    update(image): update integration test for get_by_id method from ImageDbRepository class to contain new account_id argument

--- tests/repositories/test_image_db_repository.py
+++ tests/repositories/test_image_db_repository.py
@@ -13,7 +13,7 @@ async def test_save(image_db_repo, image_obj, saved_account_obj):
 @pytest.mark.anyio
 @pytest.mark.integration
 async def test_get_by_id(image_db_repo, saved_image_obj):
-  result = await image_db_repo.get_by_id(saved_image_obj.id)
+  result = await image_db_repo.get_by_id(saved_image_obj.id, saved_image_obj.account_id)
 
   assert result == saved_image_obj
 
