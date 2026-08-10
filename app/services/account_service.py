90f7e4b94e1621df8d42119e9653219efbffa652
Eeernest <ernestgrabski16@wp.pl>
2026-08-10 19:12:10 +0200

    refactor(account): change create_account method from AccountService class by adding private methods for better readability

--- app/services/account_service.py
+++ app/services/account_service.py
@@ -1,6 +1,6 @@
 from sqlalchemy.exc import IntegrityError
 
-from app.core.exceptions import AppBaseException, UsernameUnavailableException, EmailUnavailableException
+import app.core.exceptions as e
 from app.core.security import Security
 from app.models.account_model import Account
 from app.repositories.account_db_repository import AccountDbRepository
@@ -12,28 +12,39 @@ class AccountService:
     self.db_repo = db_repo
 
   async def create_account(self, account_data: AccountCreate) -> Account:
-    if await self.db_repo.get_by_username(account_data.username) is not None:
-      raise UsernameUnavailableException(f"Username '{account_data.username}' is already in use")
-    
-    if await self.db_repo.get_by_email(account_data.email) is not None:
-      raise EmailUnavailableException(f"Email '{account_data.email}' is already in use")
-    
-    hashed_password = await self.security.get_password_hash(account_data.password)
+    await self._check_username(account_data.username)
 
+    await self._check_email(account_data.email)
+
+    hashed_password = await self.security.get_password_hash(account_data.password)
+    
     account_obj = Account(
       username=account_data.username,
       email=account_data.email,
       hashed_password=hashed_password
     )
 
+    return await self._try_save(account_obj)
+
+
+
+  async def _check_username(self, username: str) -> None:
+    if await self.db_repo.get_by_username(username) is not None:
+      raise e.UsernameUnavailableException()
+
+  async def _check_email(self, email: str) -> None:
+    if await self.db_repo.get_by_email(email) is not None:
+      raise e.EmailUnavailableException()
+
+  async def _try_save(self, account_obj: Account) -> Account:
     try:
       return await self.db_repo.save(account_obj)
 
     except IntegrityError as exc:
       if "username" in str(exc.orig):
-        raise UsernameUnavailableException(f"Username '{account_data.username}' is already in use")
-      
+        raise e.UsernameUnavailableException()
+
       if "email" in str(exc.orig):
-        raise EmailUnavailableException(f"Email '{account_data.email}' is already in use")
-    
-    return AppBaseException("Failed to create account")
+        raise e.EmailUnavailableException()
+
+    raise e.AppBaseException()
