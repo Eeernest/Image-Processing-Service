c608c7aa70ed23920f9a8319d0f8bd97e07d7979
Eeernest <ernestgrabski16@wp.pl>
2026-08-10 19:12:51 +0200

    update(account): update tests for create_account method from AccountService class

--- tests/services/test_account_service.py
+++ tests/services/test_account_service.py
@@ -1,7 +1,7 @@
 import pytest
 from sqlalchemy.exc import IntegrityError
 
-from app.core.exceptions import UsernameUnavailableException, EmailUnavailableException
+import app.core.exceptions as e
 
 @pytest.mark.anyio
 @pytest.mark.unit
@@ -23,11 +23,11 @@ async def test_create_account_success(account_obj, mock_account_security, mock_a
 async def test_create_account_username_exception(account_obj, mock_account_db_repo, account_service, account_data):
   mock_account_db_repo.get_by_username.return_value = account_obj
 
-  with pytest.raises(UsernameUnavailableException) as exc:
+  with pytest.raises(e.UsernameUnavailableException) as exc:
     await account_service.create_account(account_data)
 
   assert exc.value.status_code == 409
-  assert f"Username '{account_data.username}' is already in use" in str(exc.value)
+  assert exc.value.detail == e.UsernameUnavailableException.detail
 
 @pytest.mark.anyio
 @pytest.mark.unit
@@ -35,11 +35,11 @@ async def test_create_account_email_exception(account_obj, mock_account_db_repo,
   mock_account_db_repo.get_by_username.return_value = None
   mock_account_db_repo.get_by_email.return_value = account_obj
 
-  with pytest.raises(EmailUnavailableException) as exc:
+  with pytest.raises(e.EmailUnavailableException) as exc:
     await account_service.create_account(account_data)
 
   assert exc.value.status_code == 409
-  assert f"Email '{account_data.email}' is already in use" in str(exc.value)
+  assert exc.value.detail == e.EmailUnavailableException.detail
 
 @pytest.mark.anyio
 @pytest.mark.unit
@@ -49,11 +49,11 @@ async def test_create_account_username_race_condition(account_obj, mock_account_
   mock_account_security.get_password_hash.return_value = account_obj.hashed_password
   mock_account_db_repo.save.side_effect = IntegrityError("stmt", "params", "username")
 
-  with pytest.raises(UsernameUnavailableException) as exc:
+  with pytest.raises(e.UsernameUnavailableException) as exc:
     await account_service.create_account(account_data)
 
   assert exc.value.status_code == 409
-  assert f"Username '{account_data.username}' is already in use" in str(exc.value)
+  assert exc.value.detail == e.UsernameUnavailableException.detail
 
 @pytest.mark.anyio
 @pytest.mark.unit
@@ -63,8 +63,8 @@ async def test_create_account_email_race_condition(account_obj, mock_account_sec
   mock_account_security.get_password_hash.return_value = account_obj.hashed_password
   mock_account_db_repo.save.side_effect = IntegrityError("stmt", "params", "email")
 
-  with pytest.raises(EmailUnavailableException) as exc:
+  with pytest.raises(e.EmailUnavailableException) as exc:
     await account_service.create_account(account_data)
 
   assert exc.value.status_code == 409
-  assert f"Email '{account_data.email}' is already in use" in str(exc.value)
+  assert exc.value.detail == e.EmailUnavailableException.detail
